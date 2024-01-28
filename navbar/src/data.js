bcf0963eaa059e607084ea361642c4024c31c04e
MLDODSON <paola@dominicansoul.com>
2024-01-27 18:30:32 -0600

    Branch 2
    Branch 2 - Initial navbar setup.

--- navbar/src/data.js
+++ navbar/src/data.js
@@ -1,52 +1,55 @@
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+import twitter from "./assets/twitter.png";
+import facebook from "./assets/facebook.png";
+import behance from "./assets/behance.png";
+import linkedin from "./assets/linkedin.png";
 
 export const links = [
-    {
-      id: 1,
-      url: '/',
-      text: 'home',
-    },
-    {
-      id: 2,
-      url: '/about',
-      text: 'about',
-    },
-    {
-      id: 3,
-      url: '/projects',
-      text: 'projects',
-    },
-    {
-      id: 4,
-      url: '/contact',
-      text: 'contact',
-    },
-    {
-      id: 5,
-      url: '/profile',
-      text: 'profile',
-    },
-  ];
-  
-  export const social = [
-    {
-      id: 1,
-      url: 'https://www.twitter.com',
-      icon: <FaFacebook />,
-    },
-    {
-      id: 2,
-      url: 'https://www.twitter.com',
-      icon: <FaTwitter />,
-    },
-    {
-      id: 3,
-      url: 'https://www.twitter.com',
-      icon: <FaLinkedin />,
-    },
-    {
-      id: 4,
-      url: 'https://www.twitter.com',
-      icon: <FaBehance />,
-    },
-  ];
+  {
+    id: 1,
+    url: '/',
+    text: 'home',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'about',
+  },
+  {
+    id: 3,
+    url: '/projects',
+    text: 'projects',
+  },
+  {
+    id: 4,
+    url: '/contact',
+    text: 'contact',
+  },
+  {
+    id: 5,
+    url: '/profile',
+    text: 'profile',
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+    icon: facebook ,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: twitter,
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+    icon: linkedin,
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    icon: behance,
+  },
+];
